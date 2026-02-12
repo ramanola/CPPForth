a981fb5a88bb4eb28ec7191fd2d4b3db74643fc0
raman <ramandeephora13o3@gmail.com>
2026-02-13 02:56:17 +0530

    Implemented push and pop

--- cpp_backend.cpp
+++ cpp_backend.cpp
@@ -14,8 +14,21 @@ using namespace std;
 
 class Stack{
 
+    float a[128];
+    int itop;
+    public:
+    Stack(){itop=-1;}//initialised empty stack
+    bool isEmpty(){return itop==-1;}//return true if stack is empty
+    bool isFull(){return itop==127;}//return true is stack is full
+    void push(float x){
+        if(isFull()==true){cout<<"Stack Overflow";}
+        else{itop++; a[itop]=x;}
+    }
+    float pop(){
+        if(isEmpty()==true){"Stack Underflow";}
+        else{return a[itop]; itop--;}
+    }
     
-
 };
 
 class Dictionary{
