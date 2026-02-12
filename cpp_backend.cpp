2a1d429219ec1a79aa6f493b7345cd8dc733f179
raman <ramandeephora13o3@gmail.com>
2026-02-13 01:49:23 +0530

    Init

---
+++ cpp_backend.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+/*
+
+1. Implement Stack
+2. Implement Dictionary
+3. Implement Forth Class
+4. Initialise Program Dictionary and Stack
+5. Implement Corresponding Forth Words
+6. Create Python UI
+
+*/
+
+class Stack{
+
+    
+
+};
+
+class Dictionary{
+
+
+
+};
+
+class Forth{
+
+
+
+};
+
+int main() {
+
+    return 0;
+
+}
